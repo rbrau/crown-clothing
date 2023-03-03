3eafcbc931159ca33eb2b56cf2c3430b26cfeede
rbrau <ruben.is.working@gmail.com>
2023-03-03 09:05:34 +0100

    First commit.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+const App = () => {
+	const categories = [
+		{
+			id: 1,
+			title: 'Hats',
+		},
+	];
+	return (
+		<div className='categories-container'>
+			{categories.map(({ title }) => (
+				<div className='category-container'>
+					{/* <img></img> */}
+					<div className='category-body-container'>
+						<h2>{title}</h2>
+						<p>Shop Now</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default App;
