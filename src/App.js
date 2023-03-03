df850ef0f4d4e2c2ac7a07c4a2e4f70e81325a43
rbrau <ruben.is.working@gmail.com>
2023-03-03 09:33:01 +0100

    Lesson 1 - finished?

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ const App = () => {
 			id: 1,
 			title: 'Hats',
 		},
+		{
+			id: 1,
+			title: 'Jackets',
+		},
+		{
+			id: 1,
+			title: 'Sneakers',
+		},
+		{
+			id: 1,
+			title: 'Men',
+		},
+		{
+			id: 1,
+			title: 'Women',
+		},
 	];
 	return (
 		<div className='categories-container'>
 			{categories.map(({ title }) => (
 				<div className='category-container'>
-					{/* <img></img> */}
+					<div className='background-image' />
 					<div className='category-body-container'>
 						<h2>{title}</h2>
 						<p>Shop Now</p>
